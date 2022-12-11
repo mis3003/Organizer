ebe7c47cc0dad0ee30adf27917a83ea5c3d7b7c3
JanBaran <janbaran532@gmail.com>
2022-12-11 17:43:02 +0100

    cos

---
+++ app/src/main/java/codewithcal/au/calendarappexample/start.java
@@ -0,0 +1,35 @@
+package codewithcal.au.calendarappexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser==null){
+                    startActivity(new Intent(start.this,LoginActivity.class));
+                }else{
+                    startActivity(new Intent(start.this, codewithcal.au.calendarappexample.MainActivity.class));
+                }
+                finish();
+            }
+        },1000);
+
+
+    }
+}
